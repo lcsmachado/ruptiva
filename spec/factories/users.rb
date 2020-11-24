e8ea7ea6ac5f8382046fc578db9da84a0e582f33
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-24 18:56:25 -0300

    Creting users_controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { "123456" }
-    password_confirmation { "123456" }
+    password { '123456' }
+    password_confirmation { '123456' }
     profile { :admin }
   end
 end
---
+++ spec/requests/api/v1/users/users_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users as :admin", type: :request do
+
+  context 'GET /api/v1/users' do
+    let(:url) { 'api/v1/users' }
+  end
+
+  context 'POST /api/v1/users' do
+    let(:url) { 'api/v1/users' }
+  end
+
+  context 'PATCH /api/v1/users' do
+    let(:url) { 'api/v1/users' }
+  end
+
+  context 'DELETE /api/v1/users' do
+    let(:url) { 'api/v1/users' }
+  end
+end
