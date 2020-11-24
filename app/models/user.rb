02c218f72691c9c0f5ae17d96d0b711dcf9eff3f
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-24 17:50:55 -0300

    Creting test for User.role

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
+  
   validates :role, presence: true
+  enum role: { admin: 0, client: 1 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_presence_of(:password_confirmation) }
+
   it { is_expected.to validate_presence_of(:role) }
+  it { is_expected.to define_enum_for(:role).with_values({ admin: 0, client: 1 })}
 end
